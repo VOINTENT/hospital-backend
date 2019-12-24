aec766614c40ef7621a551d65e78c41515bda4d4
Andrey Grishin <andrey@rcsmail.ru>
2019-12-24 15:45:21 +0300

    Added doctor, servise, POST register requests

--- hospital/appointment/serializers.py
+++ hospital/appointment/serializers.py
@@ -1,21 +1,62 @@
 import json
 
-from .models import ReceptionLine, ReceptionPlan, Service, ServiceCategory, Doctor
+from .models import *
 
 def encode(object):
-    if isinstance(object, ReceptionLine):
+    if isinstance(object, User):
         return {
             'id': object.id,
-            'reception_plan': encode(object.reception_plan),
-            'time': str(object.time)
+            'login': object.login,
+            'password': object.password,
+            'email': object.email,
+            'phone_number': object.phone_number,
+            'date_creation': str(object.date_creation),
+            'is_active': object.is_active,
         }
 
-    elif isinstance(object, ReceptionPlan):
+    elif isinstance(object, Patient):
         return {
             'id': object.id,
-            'service': encode(object.service),
-            'doctor': encode(object.doctor),
-            'date': str(object.date)
+            'user': encode(object.user),
+            'name': object.name,
+            'surname': object.surname,
+            'patronymic': object.patronymic,
+            'birth_date': str(object.birth_date),
+            'snils': object.snils,
+            'policy': object.policy,
+        }
+
+    elif isinstance(object, Doctor):
+        return {
+            'id': object.id,
+            'name': object.name,
+            'surname': object.surname,
+            'patronymic': object.patronymic,
+            'birth_date': str(object.birth_date),
+            'speciality': encode(object.speciality),
+            'department': encode(object.department),
+            'services': [encode(service) for service in object.services.all()],
+            'department': encode(object.department),
+
+        }
+
+    elif isinstance(object, Speciality):
+        return {
+            'id': object.id,
+            'name': object.name
+        }
+
+    elif isinstance(object, Department):
+        return {
+            'id': object.id,
+            'name': object.name
+        }
+
+    elif isinstance(object, Cabinet):
+        return {
+            'id': object.id,
+            'number': object.number,
+            'department': encode(object.department)
         }
 
     elif isinstance(object, Service):
@@ -33,15 +74,34 @@ def encode(object):
             'name': object.name
         }
 
-    elif isinstance(object, Doctor):
+    elif isinstance(object, Price):
         return {
             'id': object.id,
-            'name': object.name,
-            'surname': object.surname,
-            'patronymic': object.patronymic,
-            'birth_date': str(object.birth_date),
-            'speciality': str(object.speciality),
-            'department': str(object.department),
+            'service': encode(object.service),
+            'cost': object.cost,
+            'date_approval': object.date_approval,
+        }
+
+    elif isinstance(object, ReceptionPlan):
+        return {
+            'id': object.id,
+            'service': encode(object.service),
+            'doctor': encode(object.doctor),
+            'date': str(object.date)
+        }
+
+    elif isinstance(object, ReceptionLine):
+        return {
+            'id': object.id,
+            'reception_plan': encode(object.reception_plan),
+            'time': str(object.time)
+        }
+
+    elif isinstance(object, Register):
+        return {
+            'id': object.id,
+            'reception_line': encode(object.reception_line),
+            'patient': encode(object.patient)
         }
 
     else:
--- hospital/appointment/urls.py
+++ hospital/appointment/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 
-from .views import reception_lines, registers
+from .views import *
 
 urlpatterns = [
-    path ('reception-lines/', reception_lines),
-    path ('registers/', registers),
+    path ('patients', patients),
+    path ('doctors', doctors),
+    path ('services', services),
+    path ('reception-lines', reception_lines),
+    path ('registers', registers),
+
 ]
